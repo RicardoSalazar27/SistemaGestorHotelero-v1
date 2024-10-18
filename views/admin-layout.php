06562a8085044492de3920d9e5dc60dc1c13b11b
RicardoSalazar <edgar_ss2012@hotmail.com>
2024-10-18 06:08:26 -0600

    Se corrige versiones de librerias e importaciones en admin-layout

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | <?php echo $titulo;?></title>
 
-    <!-- Fuente de Google Fonts (Source Sans Pro) -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="/build/resources/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/build/resources/dist/css/adminlte.min.css">
 
-    <!-- Importación de FontAwesome para los íconos -->
-    <link rel="stylesheet" href="/build/resources/plugins/fontawesome-free/css/all.min.css">
-
-    <!-- Hoja de estilo principal de AdminLTE -->
-    <link rel="stylesheet" href="/build/resources/dist/css/adminlte.min.css?v=3.2.0">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -23,16 +20,15 @@
         <!-- Header -->
         <?php include_once __DIR__ .'/templates/header.php'; ?>
 
-        <!-- SIdebar -->
+        <!-- Sidebar -->
         <?php include_once __DIR__ .'/templates/sidebarmenu.php'; ?>
 
-        <!-- Contenido principal del sitio -->
+        <!-- Contenido principal -->
         <div class="content-wrapper">
-            <!-- Imprime el contenido dinámico de la página actual -->
             <?php echo $contenido; ?>
         </div>
 
-        <!-- Sidebar derecho opcional (Control Sidebar) -->
+        <!-- Sidebar derecho opcional -->
         <aside class="control-sidebar control-sidebar-dark">
             <div class="p-3">
                 <h5>Title</h5>
@@ -44,14 +40,13 @@
         <?php include_once __DIR__ .'/templates/footer.php'; ?>
     </div>
 
-    <!-- Script para jQuery, necesaria para ciertas funcionalidades de AdminLTE -->
+    <!-- jQuery -->
     <script src="/build/resources/plugins/jquery/jquery.min.js"></script>
 
-    <!-- Script para Bootstrap, necesario para los componentes interactivos como modales, alertas, etc. -->
+    <!-- Bootstrap 4 -->
     <script src="/build/resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Script principal de AdminLTE, incluye las funcionalidades de la plantilla -->
-    <script src="/build/resources/dist/js/adminlte.min.js?v=3.2.0"></script>
+    <!-- AdminLTE App para la versión 3.1.0 -->
+    <script src="/build/resources/dist/js/adminlte.min.js"></script>
 </body>
-
 </html>
