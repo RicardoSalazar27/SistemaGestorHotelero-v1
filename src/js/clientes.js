a3c6aa89a6838de40485dddb604c282df35dd24e
RicardoSalazar <edgar_ss2012@hotmail.com>
2024-10-23 12:22:50 -0600

    ya se puede actualizar ventana modal con datos del cliente

--- src/js/clientes.js
+++ src/js/clientes.js
@@ -86,10 +86,94 @@
                     <td>${cliente.documento_identidad}</td>
                     <td>${cliente.fecha_nacimiento}</td>
                     <td>
-                        <!-- Botón de editar -->
-                        <button class="btn btn-sm btn-primary">
+                        <!-- Botón de editar que abre el modal -->
+                        <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editarClienteModal${cliente.id}">
                             <i class="fa-solid fa-pen"></i>
                         </button>
+
+                        <!-- Modal Editar Cliente -->
+                        <div class="modal fade" id="editarClienteModal${cliente.id}" tabindex="-1" role="dialog" aria-labelledby="clientesModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="clientesModalLabel">Editar Cliente</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="clientes/editar?id=${cliente.id}" method="POST">
+                                    <div class="form-group">
+                                        <label for="nombre">Nombre</label>
+                                        <input 
+                                            type="text"
+                                            class="form-control"
+                                            id="nombre"
+                                            name="nombre"
+                                            value="${cliente.nombre}"
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="apellidos">Apellidos</label>
+                                        <input 
+                                            type="text"
+                                            class="form-control"
+                                            id="apellidos"
+                                            name="apellidos"
+                                            value="${cliente.apellidos}"
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="correo">Correo</label>
+                                        <input 
+                                            type="email"
+                                            class="form-control"
+                                            id="correo"
+                                            name="correo"
+                                            value="${cliente.correo}"
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="telefono">Telefono</label>
+                                        <input 
+                                            type="text"
+                                            class="form-control"
+                                            id="telefono"
+                                            name="telefono"
+                                            value="${cliente.telefono}"
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="documento_identidad">DNI</label>
+                                        <input 
+                                            type="text"
+                                            class="form-control"
+                                            id="documento_identidad"
+                                            name="documento_identidad"
+                                            value="${cliente.documento_identidad}"
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="fecha_nacimiento">Fecha de Nacimiento</label>
+                                        <input 
+                                            type="date"
+                                            class="form-control"
+                                            id="fecha_nacimiento"
+                                            name="fecha_nacimiento"
+                                            value="${cliente.fecha_nacimiento}"
+                                        />
+                                    </div>
+
+                                    <!-- Botones en el formulario -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                        <button type="submit" class="btn btn-primary">Guardar</button>
+                                    </div>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
 
                         <!-- Formulario para eliminar -->
                         <form method="POST" action="/admin/clientes/eliminar" class="d-inline">
@@ -100,6 +184,7 @@
                         </form>
                     </td>
                 `;
+
 
                 // Agrega la fila a la tabla
                 tableBody.appendChild(row);
---
+++ views/admin/clientes/modalCrear.php
@@ -0,0 +1,82 @@
+<!-- ClientesModalCrear-->
+<div class="modal fade" id="clientesModalCrear" tabindex="-1" role="dialog" aria-labelledby="clientesModalLabelCrear" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="clientesModalLabelCrear">Agregar Nuevo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="clientes/crear" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input 
+                    type="text"
+                    class="form-control"
+                    id="nombre"
+                    name="nombre"
+                    placeholder="Tu Nombre"
+                />
+            </div>
+            <div class="form-group">
+                <label for="apellidos">Apellidos</label>
+                <input 
+                    type="text"
+                    class="form-control"
+                    id="apellidos"
+                    name="apellidos"
+                    placeholder="Tus Apellidos"
+                />
+            </div>
+            <div class="form-group">
+                <label for="correo">Correo</label>
+                <input 
+                    type="email"
+                    class="form-control"
+                    id="correo"
+                    name="correo"
+                    placeholder="Tu Correo"
+                />
+            </div>
+            <div class="form-group">
+                <label for="telefono">Telefono</label>
+                <input 
+                    type="text"
+                    class="form-control"
+                    id="telefono"
+                    name="telefono"
+                    placeholder="Tu Telefono"
+                />
+            </div>
+            <div class="form-group">
+                <label for="documento_identidad">DNI</label>
+                <input 
+                    type="text"
+                    class="form-control"
+                    id="documento_identidad"
+                    name="documento_identidad"
+                    placeholder="Tu DNI"
+                />
+            </div>
+            <div class="form-group">
+                <label for="fecha_nacimiento">Fecha de Nacimiento</label>
+                <input 
+                    type="date"
+                    class="form-control"
+                    id="fecha_nacimiento"
+                    name="fecha_nacimiento"
+                />
+            </div>
+
+            <!-- Botones en el formulario -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
