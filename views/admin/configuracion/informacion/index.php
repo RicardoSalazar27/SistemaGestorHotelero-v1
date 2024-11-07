fc927622c3add25466d481f7e5f8d725b83ccdd2
RicardoSalazar <edgar_ss2012@hotmail.com>
2024-11-07 12:05:59 -0600

    se corrigue estructura de tabla de informacion del hotel

--- views/admin/configuracion/informacion/index.php
+++ views/admin/configuracion/informacion/index.php
@@ -1,15 +1,25 @@
-<div class="content-header">
+<section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="dashboard__heading"><?php echo $titulo; ?></h1>
-            </div><!-- /.col -->
+                <div class="d-flex align-items-center">
+                    <i class="nav-icon fas fa-users mr-2"></i>
+                    <h1 class="h4 mb-0"><?php echo $titulo; ?></h1>
+                </div>
+            </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Starter Page</li>
+                    <li class="breadcrumb-item active">Informacion Hotel</li>
                 </ol>
-            </div><!-- /.col -->
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
             <div class="col-12">
             <div class="card card-primary">
                     <div class="card-header">
@@ -68,6 +78,5 @@
                     </form>
                 </div>
             </div>
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
+    </div>
+</section>
